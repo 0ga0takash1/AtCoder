aae92f15afe8f793c1cd67f4cde5ac7bbf920578
小川たかふみ <ogawatakafumi2814@gmail.com>
2020-06-21 23:06:19 +0900

    ABC171

---
+++ ABC171/s/d/d.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef int64_t i6;
+typedef std::vector<std::vector<int64_t> > Graph;
+
+#define rep(i, n) for (int64_t i = 0; i < n; ++i)
+#define rep2(i, n) for (int64_t i = 1; i <= n; ++i)
+#define ALL(v) (v).begin(), (v).end()
+#define Sort(x) sort(ALL(x))
+#define Sort_rev(x) Sort(x);reverse(ALL(x))
+#define Push_back(p, a, b) (p).push_back( make_pair( (a), (b) ) )
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1;  } return 0;  }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1;  } return 0;  }
+template<typename V,typename T> bool find_num(V v, T num) { if ( find(ALL(v), num) == v.end() ) { return false; } return true; }
+
+// π M_PI
+// deg = rad*180/M_PI
+
+int main() {
+    int64_t n;
+    cin >> n;
+    std::vector<int64_t> a(n);
+    // std::vector<int64_t> num(100000, 0);
+    rep(i, n) {
+        cin >> a[i];
+        // num[a[i]-1]++;
+    }
+    int64_t q;
+    cin >> q;
+    int64_t b[n], c[n];
+    rep(i, n) {
+        cin >> b[i] >> c[i];
+    }
+/*
+    rep(i, q) {
+        int64_t ans = accumulate(ALL(a), 0LL);
+        // cout << "a" << ans << endl;
+        int64_t C = count(ALL(a), b[i]);
+        if ( C > 0 ) {
+            ans += (c[i]-b[i])*C;
+        }
+        cout << ans << endl;
+    }*/
+
+
+    rep(i, q) {
+        // std::vector<int64_t> v;
+        // cout << "a" << ans << endl;
+        /*rep(j, n) {
+            v.push_back(a[j]);
+        }*/
+        Sort(a);
+        int64_t C = count(ALL(a), b[i]);
+        int64_t f = *find(ALL(a), b[i]);
+        rep(j, C) {
+            a[f+j] = c[i];
+        }
+/*
+        if ( C > 0 ) {
+            ans += (c[i]-b[i])*C;
+        }*/
+        int64_t ans = accumulate(ALL(a), 0LL);
+        cout << ans << endl;
+    }
+
+    return 0;
+}
---
+++ practice/One_Quadrillion_and_One_Dalmatians.cpp
@@ -0,0 +1,77 @@
+/*---------------------------------------------*/
+/*
+    ABC171 C
+ */
+/*---------------------------------------------*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef int64_t i6;
+typedef std::vector<std::vector<int64_t> > Graph;
+
+#define rep(i, n) for (int64_t i = 0; i < n; ++i)
+#define rep2(i, n) for (int64_t i = 1; i <= n; ++i)
+#define ALL(v) (v).begin(), (v).end()
+#define Sort(x) sort(ALL(x))
+#define Sort_rev(x) Sort(x);reverse(ALL(x))
+#define Push_back(p, a, b) (p).push_back( make_pair( (a), (b) ) )
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1;  } return 0;  }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1;  } return 0;  }
+template<typename V,typename T> bool find_num(V v, T num) { if ( find(ALL(v), num) == v.end() ) { return false; } return true; }
+
+// π M_PI
+// deg = rad*180/M_PI
+
+const string abc = "abcdefghijklmnopqrstuvwxyz";
+
+int main() {
+    int64_t n;
+    cin >> n;
+
+    if ( n <= abc.length()) {
+        cout << abc.at(n-1) << endl;
+        return 0;
+    }
+    std::vector<int64_t> v, v2;
+    v.push_back(abc.length());
+    // cout << "cc" << endl;
+
+    int64_t num = abc.length();
+    v.push_back(1);
+    v2.push_back(abc.length());
+    while (1) {
+        rep(i, v.size()) {
+            num *= abc.length();
+        }
+        v.push_back(num);
+        v2.push_back(v2[v2.size()-1]+num);
+        if ( v2[v2.size()-1] >= n ) break;
+        // if ( accumulate(ALL(v), 0LL) >= n ) break;
+    }
+
+    // cout << "aa" << endl;
+    int64_t dig = v.size()-1;
+    int64_t num2 = n-v2.at(v2.size()-2);
+    rep(i, dig) {
+        // cout << "bb" << endl;
+        if ( i == dig-1) {
+            // cout << abc[n%abc.length()] << endl;
+            cout << abc.at((n-1)%abc.length()) << endl;
+        } else {
+            cout << abc.at((num2-1)%( v.at( v.size()-(i+1)) ));
+        }
+    }
+
+/*
+    Graph G(26);
+    rep(i, 26) {
+        rep(j, 26) {
+            G[i].push_back(j);
+        }
+    }
+    */
+    return 0;
+}
