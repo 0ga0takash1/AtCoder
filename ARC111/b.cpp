875024fdd4c9494d6a0d1ab352a5f77f13466700
0ga0takash1 <ogawatakafumi2814@gmail.com>
2021-01-09 21:53:47 +0900

    ARC111

---
+++ ARC111/b.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef std::vector<std::vector<int64_t> > Graph;
+
+#define rep(i, n) for (int64_t i = 0; i < n; ++i)
+#define rep2(i, n) for (int64_t i = 1; i <= n; ++i)
+#define repb(i, l, n) for (int64_t i = l; i < n; ++i)
+#define repb2(i, l, n) for (int64_t i = l; i <= n; ++i)
+#define repc(i, l, n, d) for (int64_t i = l; i < n; i+=d)
+#define repc2(i, l, n, d) for (int64_t i = l; i <= n; i+=d)
+#define repi(a, b) for (auto&(a) : (b))
+#define ALL(v) (v).begin(), (v).end()
+#define Sort(x) sort(ALL(x))
+#define Sort_rev(x) Sort(x);reverse(ALL(x))
+#define Sort_pair(x, p) sort(ALL(x), (p))
+#define mp(a, b) make_pair((a), (b))
+#define Push_back(a, b) push_back( mp( (a), (b) ) )
+#define ctoi(c) (c)-'0'
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1;  } return 0;  }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1;  } return 0;  }
+template<typename V,typename T> bool find_num(V v, T num) { if ( find(ALL(v), num) == v.end() ) { return false; } return true; }
+
+const int inf = 0x3fffffff;
+const int64_t INF = 0x3fffffffffffffff;
+const int64_t MOD = 1e9+7;
+
+bool pair_Cf(const pair<int64_t, int64_t>& firstElof, const pair<int64_t, int64_t>& secondElof) {
+    return firstElof.first < secondElof.first;
+}
+
+int main() {
+    int64_t n;
+    cin >> n;
+    std::vector<int64_t> a(n), b(n);
+    /*
+    std::vector<int64_t> v(400000, 0);
+    vector<pair<int64_t, int64_t> > p;
+    */
+    rep(i, n) {
+        cin >> a[i] >> b[i];
+        /*
+        --a[i];--b[i];
+        p.Push_back(a[i], b[i]);
+        ++v[a[i]];++v[b[i]];
+        */
+    }
+
+
+    return 0;
+}
