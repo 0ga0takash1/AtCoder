b7c0ee8770f808be6225993a943a51dd26fd0797
0ga0takash1 <ogawatakafumi2814@gmail.com>
2021-04-11 23:04:57 +0900

    ABC198

---
+++ ARC116/b.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef std::vector<std::vector<int64_t> > Graph;
+
+#define in1(n) int64_t n;cin >> n;
+#define in2(n, m) int64_t n, m;cin >> n >> m;
+#define in3(n, m, k) int64_t n, m, k;cin >> n >> m >> k;
+#define in4(a, b, c, d) int64_t a, b, c, d;cin >> a >> b >> c >> d;
+#define rep(i, n) for (int64_t i = 0; i < n; ++i)
+#define rep2(i, n) for (int64_t i = 1; i <= n; ++i)
+#define repb(i, l, n) for (int64_t i = l; i < n; ++i)
+#define repb2(i, l, n) for (int64_t i = l; i <= n; ++i)
+#define repc(i, l, n, d) for (int64_t i = l; i < n; i+=d)
+#define repc2(i, l, n, d) for (int64_t i = l; i <= n; i+=d)
+#define rep_(i, r) for (int64_t i = r; i > 0; --i)
+#define rep_2(i, r) for (int64_t i = r; i >= 0; --i)
+#define rep_b(i, r, l) for (int64_t i = r; i > l; --i)
+#define rep_b2(i, r, l) for (int64_t i = r; i >= l; --i)
+#define rep_c(i, r, l, d) for (int64_t i = r; i > l; i-=d)
+#define rep_c2(i, r, l, d) for (int64_t i = r; i >= l; i-=d)
+#define repf(i, l, c, d) for (int64_t i = l; c; i+=d)
+#define repi(a, b) for (auto&(a) : (b))
+#define ALL(v) (v).begin(), (v).end()
+#define Sort(x) sort(ALL(x))
+#define Sort_rev(x) Sort(x);reverse(ALL(x))
+#define Sort_pair(x, p) sort(ALL(x), (p))
+#define mp(a, b) make_pair((a), (b))
+#define Push_back(a, b) push_back( mp( (a), (b) ) )
+#define ctoi(c) ((c)-'0')
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1;  } return 0;  }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1;  } return 0;  }
+template<typename V,typename T> bool find_num(V v, T num) { if ( find(ALL(v), num) == v.end() ) { return false; } return true; }
+const int inf = 0x3fffffff;
+const int64_t INF = 0x3fffffffffffffff;
+const int64_t MOD = 998244353;
+
+int64_t ans = 0;
+std::vector<int64_t> a;
+
+int64_t Mod(int64_t a, int64_t mod) {
+    return (a%mod + mod)%mod;
+}
+
+int64_t pow_mod(int64_t x, int64_t n, int64_t mod) {
+    int64_t res = 1;
+    while(n > 0) {
+        if (n & 1) {
+            res = res*x%mod;
+        }
+        x = x*x%mod;
+        n >>= 1;
+    }
+    return res;
+}
+
+void recursive_comb(int *indexes, int s, int rest, std::function<void(int *)> f) {
+    if (rest == 0) {
+        f(indexes);
+    } else {
+        if (s < 0) return;
+        recursive_comb(indexes, s - 1, rest, f);
+        indexes[rest - 1] = s;
+        recursive_comb(indexes, s - 1, rest - 1, f);
+    }
+}
+
+// nCkの組み合わせに対して処理を実行する
+void foreach_comb(int n, int k, std::function<void(int *)> f) {
+    int indexes[k];
+    recursive_comb(indexes, n-1, k, f);
+}
+
+int main() {
+    in1(n);
+    rep(i, n) {
+        int64_t num;
+        cin >> num;
+        a.push_back(num);
+        ans += Mod(num*num, MOD);
+    }
+    Sort(a);
+    foreach_comb(n, 2, [](int *i) {
+        ans += Mod( Mod(a[i[0]]*a[i[1]], MOD)*pow_mod(2, i[1]-i[0]-1, MOD), MOD);
+    });
+    cout << ans%MOD << endl;
+
+    return 0;
+}
