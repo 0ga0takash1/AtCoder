99cabcf0b5760d822b76fc0562ca297f0735bce5
0ga0takash1 <ogawatakafumi2814@gmail.com>
2021-11-21 03:04:15 +0900

    abc228

---
+++ ABC228/d.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef std::vector<std::vector<int64_t> > Graph;
+
+#define in1(n) int64_t n;cin >> n;
+#define in2(n, m) int64_t n, m;cin >> n >> m;
+#define in3(n, m, k) int64_t n, m, k;cin >> n >> m >> k;
+#define in4(a, b, c, d) int64_t a, b, c, d;cin >> a >> b >> c >> d;
+#define rep(i, n) for (int64_t i = 0; i < n; ++i)
+#define rep2(i, n) for (int64_t i = 1; i <= n; ++i)
+#define repb(i, l, n) for (int64_t i = l; i < n; ++i)
+#define repb2(i, l, n) for (int64_t i = l; i <= n; ++i)
+#define repc(i, l, n, d) for (int64_t i = l; i < n; i+=d)
+#define repc2(i, l, n, d) for (int64_t i = l; i <= n; i+=d)
+#define rep_(i, r) for (int64_t i = r-1; i >= 0; --i)
+#define rep_2(i, r) for (int64_t i = r; i > 0; --i)
+#define rep_b(i, r, l) for (int64_t i = r; i >= l; --i)
+#define rep_b2(i, r, l) for (int64_t i = r; i > l; --i)
+#define rep_c(i, r, l, d) for (int64_t i = r; i >= l; i-=d)
+#define rep_c2(i, r, l, d) for (int64_t i = r; i > l; i-=d)
+#define repf(i, l, c, d) for (int64_t i = l; c; i+=d)
+#define repi(a, b) for (auto&(a) : (b))
+#define ALL(v) (v).begin(), (v).end()
+#define Sort(x) sort(ALL(x))
+#define Sort_rev(x) Sort(x);reverse(ALL(x))
+#define Sort_pair(x, p) sort(ALL(x), (p))
+#define vector2(a, n, k) std::vector<std::vector<int64_t>> a(n, std::vector<int64_t>(k))
+#define ctoi(c) ((c)-'0')
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1;  } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1;  } return 0; }
+template<typename V,typename T> bool find_num(V v, T num) { if ( find(ALL(v), num) == v.end() ) { return false; } return true; }
+template<typename map,typename T> bool find_map(map m, T num) { auto itr = m.find(num); if ( itr == m.end() ) { return false; } return true; }
+
+const int inf = 0x3fffffff;
+const int64_t INF = 0x3fffffffffffffff;
+const int64_t MOD = 1<<20;
+
+int64_t h_num ( vector<int64_t> v, int64_t search ) {
+    if ( v.back() < search ) {
+        return 0;
+    }
+    int64_t ok = 0, ng = v.size();
+    while ( ok+1 < ng ) {
+        int64_t num = (ok+ng)/2;
+        if ( v[num] < search ) ok = num;
+        else ng = num;
+    }
+    return ok%MOD;
+}
+
+int main() {
+    vector<int64_t> a(MOD, -1), rem(MOD);
+    iota(ALL(rem), 0);
+    in1(q);
+    rep(i, q) {
+        in2(t, x);
+        if ( t == 1 ) {
+            int64_t h = h_num(rem, x);
+            // while ( a[h%MOD] != -1 ) ++h;
+            a[h] = x;
+            rem.erase(rem.begin()+h);
+        } else {
+            cout << a[x%MOD] << endl;
+        }
+    }
+    return 0;
+}
